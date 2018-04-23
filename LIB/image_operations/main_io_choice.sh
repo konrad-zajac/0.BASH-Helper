a79546602ac5b543a131604f76201aa6fa55f495
Konrad Zajac <konrad117@gmail.com>
2018-04-23 14:08:30 +0200

    minor fixes

--- LIB/image_operations/main_io_choice.sh
+++ LIB/image_operations/main_io_choice.sh
@@ -1,6 +1,6 @@
 
 function main_io_choice
 {
-echo -e "what to do with the photo?\n[9] - merge\n[8] - crop one image (1 format 1 image)\n[7] - crop many images from many photos (one format many images)\n[6] - crop many images from one image (many formats one image)\n[5] - crop on size from many photos[Q] - Quit"
+echo -e "what to do with the image?\n[9] - merge\n[8] - (one  format  one  image ) crop one image \n[7] - (one  format  many images) crop many images from many images \n[6] - (many formats one  image ) crop many images from one image \n[Q] - Quit"
         read image_choice
 }
